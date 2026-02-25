1ba19920ba6abc2080c99e416c51707fc1899b8c
Abhinavanand11 <abhinavanand59096@gmail.com>
2026-02-25 14:55:57 +0530

    change train.py

--- train.py
+++ train.py
@@ -9,8 +9,9 @@ from argparse import ArgumentParser
 import logging
 import numpy as np
 import h5py
-import pycbc.waveform, pycbc.noise, pycbc.psd, pycbc.distributions, pycbc.detector
 pycbc.HAVE_CUDA = False
+import pycbc.waveform, pycbc.noise, pycbc.psd, pycbc.distributions, pycbc.detector
+
 import lal
 import os, os.path
 from tqdm import tqdm
