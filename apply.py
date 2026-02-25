2413b7e7b25cd0eaf844433dc693d9b924cb359b
Abhinavanand11 <abhinavanand59096@gmail.com>
2026-02-25 14:58:18 +0530

    change apply.py

--- apply.py
+++ apply.py
@@ -7,10 +7,10 @@ from argparse import ArgumentParser
 import logging
 import numpy as np
 import h5py
-pycbc.HAVE_CUDA = False
+
 import pycbc.waveform, pycbc.noise, pycbc.psd, pycbc.distributions, pycbc.detector
 import pycbc.types
-
+pycbc.HAVE_CUDA = False
 import os, os.path
 from tqdm import tqdm
 import multiprocessing as mp
